c857d7897dc0de850131a7f3efddfb305ae2e2e8
MSScorpionCode <msscorpioncode@gmail.com>
2017-05-18 16:26:25 +0200

    writing array

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!");
+            var s = Console.ReadLine();
+            string[] commands = new string[3] { "Command1", "Command2", "Command3" };
+
+            if (s == "Hello")
+            {
+                Console.WriteLine("What is your name?");
+                var name = Console.ReadLine();
+                if (name == "Sabrina")
+                {
+                    Console.WriteLine("Heey Sabrina! My owner is called Sabrina.");
+                }
+                else
+                {
+                    Console.WriteLine("Nice To meet you " + name);
+                }
+                
+                Console.WriteLine("My name is Dakoda.");
+                Console.WriteLine("What are you here for?");
+                var here = Console.ReadLine();
+                if (here == "Testing")
+                {
+                    Console.WriteLine("Then you are in the right place!");
+                    Console.WriteLine("Tell me what can I do for you?");
+                    var input = Console.ReadLine();
+                    if (input == "Show me your commands")
+                    {
+                        Console.WriteLine(string.Join(",", commands));
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("I said Hello");
+            }
+            var key = System.Console.ReadKey();
+        }
+    }
+}
